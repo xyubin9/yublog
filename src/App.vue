8bd4277c8e20c26c519264994300bc32c2101073
xyubin <983847647@qq.com>
2022-11-10 22:59:14 +0800

    我的博客

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
+  
   <div id="app">
+    <!-- 流光背景 -->
+    <div class="background">
+        <canvas id="startrack"></canvas>
+        <div class="cover"></div>
+    </div>
+    <!-- 导航列表 -->
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <a href="http://api.wqwlkj.cn/wqwlapi/qq_friend.php?qq=983847647">QQ</a>
     </nav>
+    <!-- 音乐播放器 -->
+    <audio autoplay>
+      <source src="https://api.uomg.com/api/rand.music?sort=热歌榜" type="audio/mpeg">
+    </audio>
+    
     <router-view/>
+   
   </div>
 </template>
 
@@ -13,8 +27,8 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  /* text-align: center; */
+  /* color: #2c3e50; */
 }
 
 nav {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,259 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 主体 -->
+    <div class="main">
+        <div class="ch intro">
+            <div class="container">
+              <div class="hello">
+                <h1 id="slogan">思考中...</h1>
+                <h2>
+                  <div class="circle"> <span></span> <span></span>
+                    <span></span>
+                  </div>朝晞|朝晞网络
+                </h2>
+              </div>
+            </div>
+        </div>
+    </div>
+    <div class="ch about">
+        <div class="container">
+          <h2 class="chtitle">Who is <span>Zhaoxi</span>？</h2>
+          <div class="clear">
+            <div class="introduct">
+              <img class="avatar" src="http://q1.qlogo.cn/g?b=qq&nk=1135234221&s=640">
+              <P>坚硬的代替了那些柔软的，清晰的取代了那些模糊的。那些滑溜溜和黏糊糊的东西，带你来到了世界的角落。</p>
+              <p style="padding-bottom:1em;">How lucky to meet you!</p>
+              <p>你好，这里是朝晞 <sup>Zhaoxi</sup> ，21岁，是学生，对有趣的世界和可能有趣的你感到好奇，热爱Tech和ACG文化，正在尝试从事互联网产品/Web开发相关工作。</p>
+              <p>希望能与你在比特之海的繁星之下相见！</p>
+              <div class="chatbox">
+                <div class="line loading"> <span></span>
+                  <span></span> <span></span>
+                </div>
+              </div>
+            </div>
+            <ul class="skill clear">
+              <li>
+                <p>HTML5/CSS3</p>
+                <div class="progress">
+                  <div style="width:60%;"></div>
+                  <span>制作网站程度的能力</span>
+                </div>
+              </li>
+              <li>
+                <p>JavaScript</p>
+                <div class="progress">
+                  <div style="width:20%;"></div>
+                  <span>使用现代轮子程度的能力</span>
+                </div>
+              </li>
+              <li>
+                <p>Java</p>
+                <div class="progress">
+                  <div style="width:50%;"></div>
+                  <span>赞美最好的语言程度的能力</span>
+                </div>
+              </li>
+              <li>
+                <p>CV大法</p>
+                <div class="progress">
+                  <div style="width:65%;">
+                  </div><span>让自己舒服程度的能力</span>
+                </div>
+              </li>
+              <li>
+                <p>世界</p>
+                <div class="progress">
+                  <div style="width:20%;"></div>
+                  <span>观测世界程度的能力</span>
+                </div>
+              </li>
+              <li>
+                <p>搞事情</p>
+                <div class="progress">
+                  <div style="width:82%;"></div>
+                  <span>让事情发生程度的能力</span>
+                </div>
+              </li>
+              <li>
+                <p>魔法</p>
+                <div class="progress">
+                  <div style="width:12%;"></div>
+                  <span>和普通人相同程度的能力</span>
+                </div>
+              </li>
+              <li>
+                <p>MoePower</p>
+                <div class="progress">
+                  <div style="width:99%;"></div>
+                  <span>成为萌二程度的能力</span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </div>
+    <div class="find ch">
+      <div class="container links">
+        <h2 class="chtitle">朝晞生活的<span>地方</span></h2>
+        <div class="clear col-3">
+          <a href="/" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#28a9e0"></div>
+              <div class="inner"> <span>博客</span> </div>
+            </div>
+          </a>
+          <a href="/" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#28a9e0"></div>
+              <div class="inner"> <span>朝晞</span> </div>
+            </div>
+          </a>
+          <a href="//om" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#28a9e0"></div>
+              <div class="inner"> <span>朝晞互联</span> </div>
+            </div>
+          </a>
+        </div>
+        <div class="clear">
+          <a href="http://api.wqwlkj.cn/wqwlapi/qq_friend.php?qq=983847647" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#28a9e0"></div>
+              <div class="inner"> <i class="icon-qq"></i>
+                <span>QQ</span>
+              </div>
+            </div>
+          </a>
+          <a href="/" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#28a9e0"></div>
+              <div class="inner"> <i class="icon-mail"></i> <span>Mail</span> </div>
+            </div>
+          </a>
+          <a href="https:// 网站/" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#e6162d"></div>
+              <div class="inner"> <i class="icon-weibo"></i> <span>Weibo</span> </div>
+            </div>
+          </a>
+          <a href="/" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#f9a2a8"></div>
+              <div class="inner"> <i class="icon-bilibili"></i>
+                <span>Bilibili</span>
+              </div>
+            </div>
+          </a>
+          <a href="/" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#28a9e0"></div>
+              <div class="inner"> <i class="icon-twitter"></i> <span>Twitter</span> </div>
+            </div>
+          </a>
+          <a href="/" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#0f88eb"></div>
+              <div class="inner"> <i class="icon-zhihu"></i>
+                <span>知乎</span>
+              </div>
+            </div>
+          </a>
+          <a href="/" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#f09199"></div>
+              <div class="inner"> <i class="icon-bookmark"></i> <span>Bangumi</span> </div>
+            </div>
+          </a> <a href="http://music.163.com" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#f12d35"></div>
+              <div class="inner"> <i class="icon-netease_music"></i>
+                <span>网易云音乐</span>
+              </div>
+            </div>
+          </a>
+          <a href="https://github.com" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#353535"></div>
+              <div class="inner"> <i class="icon-github"></i> <span>Github</span> </div>
+            </div>
+          </a>
+          <a href="httpm/" target="_blank">
+            <div class="item">
+              <div class="bg" style="background-color:#b600ff"></div>
+              <div class="inner"> <i class="icon-instagram"></i> <span>Instgram</span> </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="gate ch">
+            <div class="container links">
+              <h2 class="chtitle">朝晞的<span>邻居</span>们</h2>
+              <div class="clear"> 
+                <a href="http://api.wqwlkj.cn/wqwlapi/qq_friend.php?qq=983847647" target="_blank">
+                  <div class="item">
+                    <div class="avatar"> <img src="http://q1.qlogo.cn/g?b=qq&nk=983847647&s=160"> </div>
+                    <div class="inner">
+                      <h5>Y.</h5>
+                      <p>什么都没有留下</p>
+                    </div>
+                  </div>
+                </a>
+                <a href="http://api.wqwlkj.cn/wqwlapi/qq_friend.php?qq=417312498" target="_blank">
+                  <div class="item akarin">
+                    <div class="avatar"> <img src="http://q1.qlogo.cn/g?b=qq&nk=417312498&s=160"> </div>
+                    <div class="inner">
+                      <h5>L.</h5>
+                      <p>可靠的魔法少年</p>
+                    </div>
+                  </div>
+                </a>
+                <a href="/" target="_blank">
+                  <div class="item">
+                    <div class="avatar">  </div>
+                    <div class="inner">
+                      <h5>卜卜口</h5>
+                      <p>无处不在的美味卜卜</p>
+                    </div>
+                  </div>
+                </a>
+                <a href="/" target="_blank">
+                  <div class="item">
+                    <div class="avatar">  </div>
+                    <div class="inner">
+                      <h5>三千院の暗</h5>
+                      <p>人生赢家小暗</p>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div> 
+          </div>
+
+    <!-- 底部 -->
+    <div class="footer ch">
+      <div class="container">
+        <h3>朝晞网络工作室</h3>
+        <p>蘑菇森林的尽头，新世界的起点</p>
+        <p class="c"><a href="/">Say hello to me @ </a></p>
+      </div>
+    </div>
+    
   </div>
 </template>
 
+<style>
+
+</style>
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
